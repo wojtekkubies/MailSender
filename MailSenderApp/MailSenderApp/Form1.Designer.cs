fd6a1a5b7acaf58f4511eaee5bc43c705e15c04e
wojtekkubies <wojtekkubies@gmail.com>
2015-05-06 20:49:04 +0200

    Dodanie ladowania Excel

--- MailSenderApp/MailSenderApp/Form1.Designer.cs
+++ MailSenderApp/MailSenderApp/Form1.Designer.cs
@@ -28,12 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.SendColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zamknijToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.wczytajDaneZXLSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.SendColumn});
+            this.dataGridView1.Location = new System.Drawing.Point(1, 92);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(734, 282);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // SendColumn
+            // 
+            this.SendColumn.HeaderText = "Wyslij";
+            this.SendColumn.Name = "SendColumn";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(736, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wczytajDaneZXLSToolStripMenuItem,
+            this.zamknijToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // zamknijToolStripMenuItem
+            // 
+            this.zamknijToolStripMenuItem.Name = "zamknijToolStripMenuItem";
+            this.zamknijToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.zamknijToolStripMenuItem.Text = "Zamknij";
+            this.zamknijToolStripMenuItem.Click += new System.EventHandler(this.zamknijToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // wczytajDaneZXLSToolStripMenuItem
+            // 
+            this.wczytajDaneZXLSToolStripMenuItem.Name = "wczytajDaneZXLSToolStripMenuItem";
+            this.wczytajDaneZXLSToolStripMenuItem.Size = new System.Drawing.Size(188, 22);
+            this.wczytajDaneZXLSToolStripMenuItem.Text = "Wczytaj Dane z Excela";
+            this.wczytajDaneZXLSToolStripMenuItem.Click += new System.EventHandler(this.wczytajDaneZXLSToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(488, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Szablony";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(488, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Wyślij Email";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(624, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(624, 27);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Szukaj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 374);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn SendColumn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zamknijToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem wczytajDaneZXLSToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- MailSenderApp/MailSenderApp/Form1.cs
+++ MailSenderApp/MailSenderApp/Form1.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +18,86 @@ namespace MailSenderApp
         public Form1()
         {
             InitializeComponent();
+        }
+        
+        DataTable dt = new DataTable();
+        BindingSource bd = new BindingSource();
+        private void zamknijToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void wczytajDaneZXLSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Excel File (*.xlsx)|*.xlsx";
+            openFileDialog1.FilterIndex = 1;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (ExcelPackage pck = new ExcelPackage())
+                    {
+                        // Open the Excel file and load it to the ExcelPackage
+                        using (var stream = File.OpenRead(openFileDialog1.FileName))
+                        {
+                            pck.Load(stream);
+                        }
+
+                        ExcelWorksheet ws = pck.Workbook.Worksheets.First();
+                        dt = WorksheetToDataTable(ws, true);
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Import failed. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private DataTable WorksheetToDataTable(ExcelWorksheet ws, bool hasHeader = true)
+        {
+            dt = new DataTable(ws.Name);
+            int totalCols = ws.Dimension.End.Column;
+            int totalRows = ws.Dimension.End.Row;
+            int startRow = hasHeader ? 2 : 1;
+            ExcelRange wsRow;
+            DataRow dr;
+            foreach (var firstRowCell in ws.Cells[1, 1, 1, totalCols])
+            {
+                dt.Columns.Add(hasHeader ? firstRowCell.Text : string.Format("Column {0}", firstRowCell.Start.Column));
+            }
+
+            for (int rowNum = startRow; rowNum <= totalRows; rowNum++)
+            {
+                wsRow = ws.Cells[rowNum, 1, rowNum, totalCols];
+                dr = dt.NewRow();
+                foreach (var cell in wsRow)
+                {
+                    dr[cell.Start.Column - 1] = cell.Text;
+                }
+
+                dt.Rows.Add(dr);
+            }           
+            return dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int j = 0; j < dataGridView1.Rows.Count; j++ )
+            {
+                for (int i = 1; i < dataGridView1.Rows[j].Cells.Count; i++)
+                {
+                    if (dataGridView1.Rows[j].Cells.Count == 1) continue;
+                    MessageBox.Show(dataGridView1.Rows[j].Cells[i].Value.ToString());
+                }
+            }
         }
     }
 }
